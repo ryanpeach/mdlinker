9da56c65c575a47297c5196519b147d275aee05b
Ryan Peach <rgpeach10@gmail.com>
2024-10-09 13:42:33 -0400

    New clippy settings

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,3 @@
-#![warn(clippy::pedantic)]
-#![warn(clippy::panic)]
-#![warn(clippy::unwrap_used)]
-#![allow(clippy::similar_names)]
-#![allow(dead_code)]
-#![allow(clippy::missing_errors_doc)]
-
 pub mod config;
 pub mod error;
 pub mod file;
